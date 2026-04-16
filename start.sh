ca5f641aa60833bc684761fe6a2a682d2e280b1c
yairriverajul <48733165+yairriverajul@users.noreply.github.com>
2026-04-16 16:06:39 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,14 +4,9 @@ set -e
 export DISPLAY=:1
 export PORT="${PORT:-10000}"
 
-mkdir -p /var/run/dbus /var/log/supervisor
-
 if [ -f /usr/share/novnc/vnc.html ] && [ ! -f /usr/share/novnc/index.html ]; then
   ln -sf /usr/share/novnc/vnc.html /usr/share/novnc/index.html
 fi
 
-echo "DISPLAY=$DISPLAY"
-echo "PORT=$PORT"
-echo "Iniciando supervisord..."
-
-exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+export PORT
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
